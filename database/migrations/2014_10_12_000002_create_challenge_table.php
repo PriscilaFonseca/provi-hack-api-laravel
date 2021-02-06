7c20117041338c947c35a25d9a2e650808e728d4
Jessica JCMZ. Caroline Milani Zambianco <jessica-zambianco@intranet.lojasmm.com.br>
2021-02-06 19:10:58 -0300

    criação de novas tabelas

--- database/migrations/2014_10_12_000002_create_challenge_table.php
+++ database/migrations/2014_10_12_000002_create_challenge_table.php
@@ -18,7 +18,7 @@ class CreateChallengeTable extends Migration
             $table->string('title',255);
             $table->string('description',1000);
             $table->integer('id_area_expertise');
-            $table->integer('participant_number ');
+            $table->integer('participant_number');
         });
     }
 
