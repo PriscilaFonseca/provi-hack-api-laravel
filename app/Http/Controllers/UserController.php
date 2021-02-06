f6d4cb133acd0604cb614a28373cfd7c92282ef3
Priscila Fonseca <pripof@gmail.com>
2021-02-06 18:47:00 -0300

    Auth user admin

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    function auth(Request $request)
+    {
+        $user= User::where('email', $request->email)->first();
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return response([
+                    'message' => ['Invalid credentials.']
+                ], 404);
+            }
+        
+            $token = $user->createToken('my-app-token')->plainTextToken;
+        
+            $response = [
+                'user' => $user,
+                'token' => $token
+            ];
+        
+            return response($response, 201);
+    }
+}
