babeeb1c245ee9f79285f213867d472fc0067d5f
Jessica JCMZ. Caroline Milani Zambianco <jessica-zambianco@intranet.lojasmm.com.br>
2021-02-06 22:37:18 -0300

    alteracao banco

--- database/migrations/2021_02_07_010841_create_users_challenge_table.php
+++ database/migrations/2021_02_07_010841_create_users_challenge_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserChallengeTable extends Migration
+class CreateUsersChallengeTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,8 +15,8 @@ class CreateUserChallengeTable extends Migration
     {
         Schema::create('users_challenge', function (Blueprint $table) {
             $table->integer('id_user');
-            $table->integer('id_challenger');
-            $table->string('user_challenges',255);
+            $table->integer('id_challenge');
+            $table->timestamps();
         });
     }
 
